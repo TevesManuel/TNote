3dffb67b176ffc7e097eac6ab870024b408c3127
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-02-21 12:15:51 -0300

    Translation feature

--- Scripts/drawing.gd
+++ Scripts/drawing.gd
@@ -1,14 +1,88 @@
 extends Object
 class_name Drawing
 
+const OFFSET := 10
+
 var line: Line2D
 var center: Vector2
 var radius: float
+var width: float
 
-func _init(l: Line2D, p: Vector2, r: float):
+var maxY := 0
+var minY := -1
+var maxX := 0
+var minX := -1
+
+var selectDrawer
+
+func _getCenterOfDraw(d: Line2D) -> Vector2:
+	var sumX := 0
+	var sumY := 0
+	maxY = 0
+	minY = -1
+	maxX = 0
+	minX = -1
+	for p in d.get_points():
+		print(p)
+		if minY == -1:
+			minY = p.y
+		if minX == -1:
+			minX = p.x
+		sumX += p.x
+		sumY += p.y
+		if p.x > maxX:
+			maxX = p.x
+		if p.x < minX:
+			minX = p.x
+		if p.y > maxY:
+			maxY = p.y
+		if p.y < minY:
+			minY = p.y
+
+	return Vector2(sumX/d.get_point_count(), sumY/d.get_point_count())
+
+func _getRadius(d: Line2D, c: Vector2) -> float:
+	var maxDistance := 0.0
+	var dist := 0
+	for p in d.get_points():
+		dist = p.distance_to(c)
+		if dist > maxDistance:
+			maxDistance = dist
+	return maxDistance
+
+func _init(l: Line2D):
 	line = l
-	center = p
-	radius = r
+	center = _getCenterOfDraw(line)
+	radius = _getRadius(line, center)
 
 func isClicked(p: Vector2) -> bool:
 	return center.distance_to(p) < radius
+
+func makeSelectDrawer() -> Line2D:
+	unselect()
+	selectDrawer = Line2D.new()
+	selectDrawer.points = [
+		Vector2(minX - OFFSET, maxY + OFFSET),
+		Vector2(maxX + OFFSET, maxY + OFFSET),
+		Vector2(maxX + OFFSET, minY - OFFSET),
+		Vector2(minX - OFFSET, minY - OFFSET),
+		Vector2(minX - OFFSET, maxY + OFFSET),
+	]
+	selectDrawer.width = 1
+	selectDrawer.default_color = Color(0, 1, 1)
+	return selectDrawer
+
+func unselect():
+	if selectDrawer != null:
+		selectDrawer.queue_free()
+
+func update():
+	for i in range(line.points.size()):
+		line.points[i] += line.position
+	line.position = Vector2.ZERO
+	_init(line)
+
+
+func setPosition(position: Vector2):
+	line.position = position - center
+	selectDrawer.position = position - center
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,61 +4,62 @@ var lastTouchPosition := Vector2.ZERO
 var lastTouchAt := 0
 
 var fingerDrawing := -1
-var drawing := false
+var isDrawing := false
 var drawer : Line2D
 
-var drawings = []
+var drawings : Array[Drawing] = []
+var selectedDraw : Drawing = null
 
-func getCenterOfDraw(d: Line2D) -> Vector2:
-	var sumX := 0
-	var sumY := 0
-	for p in d.get_points():
-		sumX += p.x
-		sumY += p.y
-	return Vector2(sumX/d.get_point_count(), sumY/d.get_point_count())
+var fingerToDrag := -1
 
-func getRadius(d: Line2D, c: Vector2) -> float:
-	var maxDistance := 0.0
-	var dist := 0
-	for p in d.get_points():
-		dist = p.distance_to(c)
-		if dist > maxDistance:
-			maxDistance = dist
-	return maxDistance
+func drawUpdate(event : InputEvent):
+	if !isDrawing:
+		isDrawing = true
+		fingerDrawing = event.index
+		var line = Line2D.new()
+		line.width = 5
+		line.default_color = Color.WHITE
+		add_child(line)
+		drawer = line
+	elif fingerDrawing == event.index:
+		drawer.add_point(event.position)
 
 func _input(event):
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			if ((Time.get_ticks_msec() - lastTouchAt) < 200) && (event.position.distance_to(lastTouchPosition) < 50):
 				print("At ", lastTouchPosition)
+			
 			for d in drawings:
 				if d.isClicked(event.position):
-					d.line.default_color = Color(1, 0, 0, 1)
+					if selectedDraw == d:
+						print("Clicked again")
+						fingerToDrag = event.index
+					else:
+						add_child(d.makeSelectDrawer())
+						selectedDraw = d
+				else:
+					selectedDraw = null
+					d.unselect()
+
 		else:
+			if fingerToDrag == event.index:
+				fingerToDrag = -1
+				selectedDraw.update()
+				selectedDraw = null
+
 			if fingerDrawing == event.index:
 				if drawer.get_point_count() > 0:
-					var center := getCenterOfDraw(drawer)
-					drawings.append(Drawing.new(drawer, center, getRadius(drawer, center)))
+					drawings.append(Drawing.new(drawer))
 				drawer = null
 				fingerDrawing = -1
-				drawing = false
+				isDrawing = false
 			lastTouchPosition = event.position
 			lastTouchAt = Time.get_ticks_msec()
+
 	elif event is InputEventScreenDrag:
-		if !drawing:
-			drawing = true
-			fingerDrawing = event.index
-			var line = Line2D.new()
-			line.width = 5
-			line.default_color = Color.WHITE
-			add_child(line)
-			drawer = line
-		elif fingerDrawing == event.index:
-			drawer.add_point(event.position)
-		#print("Arrastrando en:", event.position, " con velocidad:", event.velocity)
-
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
+		if event.index == fingerToDrag:
+			if selectedDraw != null:
+				selectedDraw.setPosition(event.position)
+		else:
+			drawUpdate(event)
